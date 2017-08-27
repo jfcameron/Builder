0d024a25cc644974403320f1950e7b4453f0ff75
Joseph Cameron <jf.cameron@gmail.com>
2017-08-26 21:23:40 -0400

    continuing work on cmake files

--- build/osx/generate.sh
+++ build/osx/generate.sh
@@ -1,4 +1,1 @@
-#find . ! -name 'generate.sh' -type f -exec rm -f {} +
-#find . ! -name '.*' -type d -exec rm -r -f {} +
-
-cmake ../../cmake/ -G "Xcode" -DPLATFORM="PLATFORM_OSX"
+cmake ../../cmake/ -G "Xcode" #-DPLATFORM="osx"
--- src/common/main.cpp
+++ src/common/main.cpp
@@ -1,18 +1,8 @@
-// A simple program that computes the square root of a number
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <iostream>
+
 int main (int argc, char *argv[])
 {
-    if (argc < 2)
-    {
-        fprintf(stdout,"Usage: %s number\n",argv[0]);
-        return 1;
-    }
+    std::cout << "Hello there!" << std::endl;
     
-    double inputValue = atof(argv[1]);
-    double outputValue = sqrt(inputValue);
-    fprintf(stdout,"The square root of %g is %g\n",
-    inputValue, outputValue);
     return 0;
 }
