f124efdc0ad9115f2cf45ff6d1bcdcf50f68a39a
Joseph Cameron <jf.cameron@gmail.com>
2017-08-27 23:17:09 -0400

    Update generate.sh

--- build/win/generate.sh
+++ build/win/generate.sh
@@ -1,1 +1,1 @@
-python ../../cmake/PreCMakelists.py -platform win\ glfw -generator Visual\ Studio\ 14\ 2015\ Win64
+python ../../cmake/PreCMakelists.py -platforms win\ glfw -generator Visual\ Studio\ 14\ 2015\ Win64
